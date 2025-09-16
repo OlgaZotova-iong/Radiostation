d815c0ae25ecaa7d5a1d68a6a33087abc6ef9f90
Olga Zotova <olgazotova1976@yandex.ru>
2025-09-16 19:01:12 +0300

    Run the tests

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,67 @@
+package ru.netology.radio;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int numberOfStations;
+
+    // Конструктор без параметров (по умолчанию)
+    public Radio() {
+        this.numberOfStations = 10; // Значение по умолчанию, если не указано иное
+    }
+
+    // Конструктор с параметром количества станций
+    public Radio(int numberOfStations) {
+        this.numberOfStations = numberOfStations;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0 || currentStation >= numberOfStations) {
+            return; // Ничего не меняем, если станция вне диапазона
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0 || currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void next() {
+        if (currentStation == numberOfStations - 1) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = numberOfStations - 1;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,301 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    //Тесты с параметрами количества станций
+    @Test
+    void shouldSetCurrentStationWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(15);
+        assertEquals(15, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldNotSetStationWhenCountInvalidTooHighWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(20);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldNotSetStationWhenCountInvalidTooLowWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void notChangeStationWhenCountInvalidTooHigh() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(15);
+        radio.setCurrentStation(22);
+        assertEquals(15, radio.getCurrentStation());
+    }
+
+    @Test
+    void notChangeStationWhenCountInvalidTooLow() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(5);
+        radio.setCurrentStation(-5);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void nextStationWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(15);
+        radio.next();
+        assertEquals(16, radio.getCurrentStation());
+    }
+    @Test
+    void shouldSwitchToFirstStationIfNextFromLastWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(19);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void prevRadioStationWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(15);
+        radio.prev();
+        assertEquals(14, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldSwitchToLastStationIfPrevFromFirstWithCustomStationsCount() {
+        Radio radio = new Radio(20);
+        radio.prev();
+        assertEquals(19, radio.getCurrentStation());
+    }
+    //Тесты без параметров
+    @Test
+    void setCurrentStationValid() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void setCurrentStationInvalidTooHigh() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void setCurrentStationInvalidTooLow() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void setCurrentStationDoesNotChangeIfInvalidHigh() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5); // Set initial state
+        radio.setCurrentStation(15);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void setCurrentStationDoesNotChangeIfInvalidLow() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5); // Set initial state
+        radio.setCurrentStation(-5);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void nextStationNotMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(4);
+        radio.next();
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void nextStationIsMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void prevStationNotMin() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(6);
+        radio.prev();
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void prevStationIsMin() {
+        Radio radio = new Radio();
+        radio.prev();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void increaseVolumeNotMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+        radio.increaseVolume();
+        assertEquals(51, radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseVolumeIsMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseVolumeNotMin() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+        radio.decreaseVolume();
+        assertEquals(49, radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseVolumeIsMin() {
+        Radio radio = new Radio();
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeValid() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(75);
+        assertEquals(75, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeInvalidTooHigh() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(101);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeInvalidTooLow() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-1);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeDoesNotChangeIfInvalidHigh() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50); // set initial state
+        radio.setCurrentVolume(105);
+        assertEquals(50, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeDoesNotChangeIfInvalidLow() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50); // set initial state
+        radio.setCurrentVolume(-5);
+        assertEquals(50, radio.getCurrentVolume());
+    }
+
+    @Test
+    void nextStationFrom8to9() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        radio.next();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    void prevStationFrom1to0() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+        radio.prev();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    // Дополнительные тесты для полного покрытия
+    @Test
+    void radioConstructorInitialStationAndVolumeAreZero() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    // Тесты для граничных значений громкости
+    @Test
+    void setVolumeToZero() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setVolumeToMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    //Тесты для граничных значений станций
+    @Test
+    void setStationToZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void setStationToMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    // Дополнительные тесты для обхода станций
+
+    @Test
+    void nextStationFromZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.next();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    void prevStationFromNine() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.prev();
+        assertEquals(8, radio.getCurrentStation());
+    }
+
+    // Дополнительные тесты для изменения громкости (граничные случаи)
+
+    @Test
+    void increaseVolumeFromMaxStaysMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseVolumeFromMinStaysMin() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+}
